4916449dffa7cba01fe803a15ebd3937cd466858
mazzwell <137079218+mazzwell@users.noreply.github.com>
2023-08-14 22:29:34 +0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import { TaikoEldfellL3 } from "@thirdweb-dev/chains";
 
 // This is the chain your dApp will work on.
 const activeChain = "mumbai";
@@ -8,7 +9,7 @@ const activeChain = "mumbai";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-      activeChain={activeChain}
+      activeChain={TaikoEldfellL3}
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
     >
       <Component {...pageProps} />
