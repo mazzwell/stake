e70daebbcd4c5fc984f9e77ecd192247bb56610f
mazzwell <137079218+mazzwell@users.noreply.github.com>
2023-08-15 09:59:31 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       {/* Top Section */}
-      <h1 className={styles.h1}>thirdweb Deploy - Custom Staking Contract</h1>
+      <h1 className={styles.h1}>STAKE YOUR NFT</h1>
       <div className={styles.nftBoxGrid}>
         <div
           className={styles.optionSelectBox}
@@ -20,7 +20,7 @@ const Home: NextPage = () => {
           <Image src="/icons/drop.webp" alt="drop" width={64} height={64} />
           <h2 className={styles.selectBoxTitle}>Mint a new NFT</h2>
           <p className={styles.selectBoxDescription}>
-            Use the NFT Drop Contract to claim an NFT from the collection.
+            Claim an NFT from the collection.
           </p>
         </div>
 
@@ -33,8 +33,8 @@ const Home: NextPage = () => {
           <Image src="/icons/token.webp" alt="token" width={64} height={64} />
           <h2 className={styles.selectBoxTitle}>Stake Your NFTs</h2>
           <p className={styles.selectBoxDescription}>
-            Use the custom staking contract deployed via <b>thirdweb Deploy</b>{" "}
-            to stake your NFTs, and earn tokens from the <b>Token</b> contract.
+           Stake your  <b>Morkie Pass</b>{" "} at fixed APR
+        
           </p>
         </div>
       </div>
