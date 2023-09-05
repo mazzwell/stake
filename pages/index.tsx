6ac24dd2d3efb905150787abe63d2df2b3a5d7de
mazzwell <137079218+mazzwell@users.noreply.github.com>
2023-09-05 12:30:51 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ const Home: NextPage = () => {
         <div
           className={styles.optionSelectBox}
           role="button"
-          onClick={() => router.push(`https://token.mokie.xyz/`)}
+          onClick={() => router.push(`/stake`)}
         >
           {/* Staking an NFT */}
           <Image src="/icons/token.webp" alt="token" width={64} height={64} />
@@ -41,7 +41,7 @@ const Home: NextPage = () => {
         <div
           className={styles.optionSelectBox}
           role="button"
-          onClick={() => router.push(`/stake`)}
+          onClick={() => router.push(`https://token.mokie.xyz/`)}
         >
           {/* Staking an NFT */}
           <Image src="/icons/tokenx.webp" alt="token" width={64} height={64} />
