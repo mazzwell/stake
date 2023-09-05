a0435eb708bb7036cbe7a883b06293609b8eff78
mazzwell <137079218+mazzwell@users.noreply.github.com>
2023-09-05 12:28:54 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,6 +37,21 @@ const Home: NextPage = () => {
         
           </p>
         </div>
+
+        <div
+          className={styles.optionSelectBox}
+          role="button"
+          onClick={() => router.push(`/stake`)}
+        >
+          {/* Staking an NFT */}
+          <Image src="/icons/tokenx.webp" alt="token" width={64} height={64} />
+          <h2 className={styles.selectBoxTitle}>Stake Mokrie</h2>
+          <p className={styles.selectBoxDescription}>
+           Stake your  <b>Mork</b>{" "} token at fixed APR
+        
+          </p>
+        </div>
+
       </div>
     </div>
   );
